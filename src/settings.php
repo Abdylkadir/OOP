9bbc207b03b3416835360aca1f58dd7ee2c02c24
Abdylkadir2004 <abdylkadir2004@gmail.com>
2022-07-11 13:46:38 +0300

    ak

---
+++ src/settings.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        'db' =>[
+            'host' => 'localhost',
+            'dbname' => 'rk3',
+            'user' => 'root',
+            'password' => '',
+        ]
+        ];
+?>
